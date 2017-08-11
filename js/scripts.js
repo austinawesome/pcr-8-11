86ada52f28860201d15649805cf6ff3ad6329852
Austin Cummings <skwerl_e@yahoo.com>
2017-08-11 16:28:01 -0700

    figure out logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
     if (!name) {
       alert("Look's Like you forgot your name");
     };
+    Acount = 0;
+    Bcount = 0;
+    Ccount = 0;
     var title = $("#title").val();
        q1 = $("input:radio[name=q1]:checked").val();
        q2 = $("input:radio[name=q2]:checked").val();
@@ -15,11 +18,20 @@ $(document).ready(function() {
        q7 = $("input:radio[name=q7]:checked").val();
        q8 = $("input:radio[name=q8]:checked").val();
 
-
+       if (q1==="a") {
+        Acount= Acount+1;
+        }
+else if (q1==="b") {
+        Bcount=Bcount+1;
+}
+ else
+Ccount= Ccount+1;
 
     var dob = $("#born").val();
 
-    var result = "thank you "+title+" "+name+", your zodiac for "+dob+" along with your choice's have revealed you're best suited for "+pathname+"." ;
+    var result = "thank you "+title+" "+name+", your zodiac for "+dob+" along with your choice's have revealed you're best suited for ." ;
+
+// alert("a "+Acount+" ");
     $("#output").text(result);
   });
-});
+  });
